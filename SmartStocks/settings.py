799f2d9814de93d429f2dc4e4bf18871a13c7f3d
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2019-04-16 22:51:36 +0530

    Added stock_model_updater.py which updates models every 10 days(hard coded)

--- SmartStocks/settings.py
+++ SmartStocks/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
---
+++ stock_model_updater.py
@@ -0,0 +1,57 @@
+import os
+from django.conf import settings
+import django
+from django.apps import apps
+
+
+
+conf = {
+    'INSTALLED_APPS': [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.messages',
+        'django.contrib.sessions',
+        'django.contrib.sitemaps',
+        'django.contrib.sites',
+        'django.contrib.staticfiles',
+        'stocks.apps.StocksConfig',
+
+    ],
+    'DATABASES': {
+        'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+          'NAME':'smartstocksdb',
+          'USER':'postgres',
+          'PASSWORD':'123456',
+          'HOST':'localhost'
+    }
+    },
+    'TIME_ZONE': 'UTC'
+}
+
+settings.configure(**conf)
+apps.populate(settings.INSTALLED_APPS)
+
+from stocks.models import Stock
+from stocks.ml import Prediction,Preprocessing
+
+
+
+
+cwd = os.getcwd()
+path = os.path.join(cwd,"ml_models")
+available_models = os.listdir(path)
+available_models = [ model[:-3] for model in available_models]
+
+
+for model in available_models:
+  current_stock = Stock.objects.get(stock_name= model)
+  Prediction(current_stock).update_model(10)
+  print("Updating model :",model)
+
+
+
+
+
+
