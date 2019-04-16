401c27815eacefe591cab5274193e5fd8ffb4163
PriteshG-IIITB <priteshRajesh.Ghogale@iiitb.org>
2019-04-16 11:54:18 +0530

    minor warnings/problem fix

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import *
+from .views import StockDetailView, StockListView,ChartData
 
 urlpatterns = [
    
 
     path('', StockListView.as_view(), name='stock_list'),
-    path('<slug:stock_name>', StockDetailView.as_view(), name='stock_detail'),
+    path('<str:stock_name>', StockDetailView.as_view(), name='stock_detail'),
     #path('/chart/api/data/<slug:stock_name>',StockDetailView.as_view(),name='stock_detail'),
     #path('api/data',views.get_data, name='api-data'),
     path('api/chart/data', ChartData.as_view()),
