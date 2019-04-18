fc6d644d1fcec81beab9d022afd70bb5ebc09b12
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2019-04-18 11:38:38 +0530

    Removed DB connection code as it can now pass stock_symbol to  Prediction and Preprocessing classes directly

--- stock_model_updater.py
+++ stock_model_updater.py
@@ -5,35 +5,35 @@ from django.apps import apps
 
 
 
-conf = {
-    'INSTALLED_APPS': [
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.messages',
-        'django.contrib.sessions',
-        'django.contrib.sitemaps',
-        'django.contrib.sites',
-        'django.contrib.staticfiles',
-        'stocks.apps.StocksConfig',
+# conf = {
+#     'INSTALLED_APPS': [
+#         'django.contrib.admin',
+#         'django.contrib.auth',
+#         'django.contrib.contenttypes',
+#         'django.contrib.messages',
+#         'django.contrib.sessions',
+#         'django.contrib.sitemaps',
+#         'django.contrib.sites',
+#         'django.contrib.staticfiles',
+#         'stocks.apps.StocksConfig',
 
-    ],
-    'DATABASES': {
-        'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-          'NAME':'smartstocksdb',
-          'USER':'postgres',
-          'PASSWORD':'123456',
-          'HOST':'localhost'
-    }
-    },
-    'TIME_ZONE': 'UTC'
-}
+#     ],
+#     'DATABASES': {
+#         'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#           'NAME':'smartstocksdb',
+#           'USER':'postgres',
+#           'PASSWORD':'123456',
+#           'HOST':'localhost'
+#     }
+#     },
+#     'TIME_ZONE': 'UTC'
+# }
 
-settings.configure(**conf)
-apps.populate(settings.INSTALLED_APPS)
+# settings.configure(**conf)
+# apps.populate(settings.INSTALLED_APPS)
 
-from stocks.models import Stock
+# from stocks.models import Stock
 from stocks.ml import Prediction,Preprocessing
 
 
@@ -42,12 +42,11 @@ from stocks.ml import Prediction,Preprocessing
 cwd = os.getcwd()
 path = os.path.join(cwd,"ml_models")
 available_models = os.listdir(path)
-available_models = [ model[:-3] for model in available_models]
+available_models = [ model[:-3] for model in available_models ]
 
 
-for model in available_models:
-  current_stock = Stock.objects.get(stock_name= model)
-  Prediction(current_stock).update_model(10)
+for model in available_models:  
+  Prediction(model).update_model(10)
   print("Updating model :",model)
 
 
