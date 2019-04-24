dfbc621b19d4633826e771f1b2a529a47d0aec08
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2019-04-25 01:47:06 +0530

    ELK stack

--- news/urls.py
+++ news/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('news/<str:stockSymbol>',views.stocknews,name='stocknews'),
+    path('news/all',views.all_news,name='all_news'),
+
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,render
 from django.http import HttpResponse
 from iex import Stock
 from . import models
@@ -6,3 +6,6 @@ from . import models
 def stocknews(request,stockSymbol):
     return render_to_response('news/stocknews.html',
     {"stocknews":Stock(stockSymbol).news(last=10),"stockname":models.getStkName(stockSymbol)})
+
+def all_news(request):
+    return render(request,"news/allnews.html")
