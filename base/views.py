c6751d636a90afea50c809e2cc4192edbd052347
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2019-04-25 02:54:36 +0530

    Minor Changes to accounts/views.py to handle new user exception

--- base/views.py
+++ base/views.py
@@ -9,8 +9,11 @@ def dashboard(request):
     user_financials = finance.Financials.objects.get(user = username)
     stocks_owned = ""    
     stocks_owned = user_financials.stocks_owned
+    print(stocks_owned)
 
-    if not stocks_owned:
+    
+    if  stocks_owned:
+        print("stocks present")
         stocks_owned=ast.literal_eval(stocks_owned)
         return render_to_response('base/dashboard.html',{"username" : username,"stocks_owned":stocks_owned})
     
